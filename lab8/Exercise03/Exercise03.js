f274c66807e3de1d621ed3ce9d0116fb11e3c22d
kizzanaome <naomekizzan@gmail.com>
2025-05-07 23:10:22 -0500

    Fix lab3 Exercise03

--- lab8/Exercise03/Exercise03.js
+++ lab8/Exercise03/Exercise03.js
@@ -1,16 +1,29 @@
-(async function  fetchRecipes (){
-    try{
-        const response = await fetch("https://dummyjson.com/recipes")
-        if (response.status === 200) {
-            const data = await response.json();
-            console.log(data)
-            const recipeNames = data.recipes.map(recipe => recipe.name)
-            console.log(recipeNames)
-        }else{
-            console.log("Failed to fetch recipes", response.status)
-        }
-    }catch(error){
-        console.log("Error Fetching recipes:", error)
-    }
+// (async function  fetchRecipes (){
+//     try{
+//         const response = await fetch("https://dummyjson.com/recipes")
+//         if (response.status === 200) {
+//             const data = await response.json();
+//             console.log(data)
+//             const recipeNames = data.recipes.map(recipe => recipe.name)
+//             console.log(recipeNames)
+//         }else{
+//             console.log("Failed to fetch recipes", response.status)
+//         }
+//     }catch(error){
+//         console.log("Error Fetching recipes:", error)
+//     }
 
-}())
+// }())
+
+console.log('start');
+async function foo() {
+    return ' done ! ';
+}
+async function bar() {
+    console.log(' insidebar - start');
+    let result = await foo();
+    console.log(result);
+    console.log('insidebar - end');
+}
+bar();
+console.log('end');
