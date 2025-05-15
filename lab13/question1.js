2fbf8b6c921ca7d06118c1a7d07074d88d290d29
kizzanaome <naomekizzan@gmail.com>
2025-05-15 00:17:06 -0500

    Add lab13 solutions

---
+++ lab13/question1.js
@@ -0,0 +1,31 @@
+
+const fs = require('fs');
+// or const fs = require('fs');
+process.nextTick(() => console.log('nextTick 1'));
+Promise.resolve().then(() => console.log('promise 1'));
+setImmediate(() => { console.log('setImmediate 1') });
+setTimeout(() => console.log('setTimeout 1'), 0);
+fs.readFile('./files/input.txt', "utf-8", (err, data) => {
+    if (err)
+        console.log('there is an error. can not read from file');
+    else {
+        console.log(data);
+    }
+});
+
+// Common js output const fs = require('fs');
+
+// nextTick 1
+// promise 1
+// setTimeout 1
+// there is an error.can not read from file
+// setImmediate 1
+
+//Es moudle  output
+
+// promise 1
+// nextTick 1
+// setImmediate 1
+// setTimeout 1
+//there is an error.can not read from file
+
